8421d5fad9f8ac98e3f0c8a314901d41da8c987e
Becca Rothwell <becca.rothwell@gmail.com>
2023-12-21 17:06:32 +0000

    test: Add initial test for successufl API response

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
 
-test("renders learn react link", () => {
+const server = setupServer();
+const successResponse = http.get("https://swapi.dev/api/people/1", () => {
+  return HttpResponse.json({ name: "Luke Skywalker" });
+});
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test(`Given the App renders, 
+      When a valid an API call is made with a successful response, 
+      Then the Response name should appear on the page.`, async () => {
+  server.use(successResponse);
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const someName = await screen.findByText("Luke Skywalker");
+
+  expect(someName).toBeInTheDocument();
 });
